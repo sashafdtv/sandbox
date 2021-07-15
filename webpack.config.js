52ffee9f4d14d1591d5e504d6fd9a7b8eff13fa7
afedotov <afedotov@team.amocrm.com>
2021-07-15 15:54:38 +0300

    Небольшая декомпозиция и допил вебпак сервера

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
+    entry: './src/index.js',
     watch: true,
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: 'index.js',
+        filename: './src/index.js',
     },
 };
