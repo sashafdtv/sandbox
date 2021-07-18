45c27f4cc80ff87ae1c4e7414b2198f63bd78ae8
sashafdtv <sashafdtv@gmail.com>
2021-07-18 21:02:37 +0300

    run lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "@vue/prettier",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-      "prettier/prettier": [
-        "warn",
-        {
-          "tabWidth": 4,
-          "singleQuote": true,
-          "endOfLine": "auto"
-        }
-      ],
-      "vue/require-default-prop": 0,
-      "vue/require-prop-types": 0
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        '@vue/prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'prettier/prettier': [
+            'warn',
+            {
+                tabWidth: 4,
+                singleQuote: true,
+                endOfLine: 'auto',
+            },
+        ],
+        'vue/require-default-prop': 0,
+        'vue/require-prop-types': 0,
+    },
 };
