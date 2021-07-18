3a279bf85a1291262cffc39532d91ecdfb4d99ad
sashafdtv <sashafdtv@gmail.com>
2021-07-18 21:00:19 +0300

    4 space = tab

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,15 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+      "prettier/prettier": [
+        "warn",
+        {
+          "tabWidth": 4,
+          "singleQuote": true,
+          "endOfLine": "auto"
+        }
+      ],
+      "vue/require-default-prop": 0,
+      "vue/require-prop-types": 0
   },
 };
